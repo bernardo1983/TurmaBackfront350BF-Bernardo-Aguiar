e762214288f9bc81d81f2a4c51b6398fd7e32cd7
Bernardo Aguiar Guimarães <bernardoguimaraes621@gmail.com>
2023-03-07 20:43:08 -0300

    criei 2 classes

--- Classe - aula/Classe - aula/main.swift
+++ Classe - aula/Classe - aula/main.swift
@@ -31,4 +31,101 @@ class Automovel {
     
 }
 
+class Bike{
+    var temPneu: Bool = true
+    var cor: String = "Azul"
+    var temBuzina: Bool = false
+    var quantidadePortaGarrafa: Int = 1
+    
+    func andar(){
+        print("Não é possível andar, pois está quebrada")
+    }
+    
+    func estacionar(){
+        print("Sempre estacione em local seguro!")
+    }
+}
 
+class Elefante{
+    var peso: Double = 1000.0
+    var cor: String = "Marrom"
+    var idade: Int = 50
+    
+    func local(){
+        print("O elefante mora na savana.")
+    }
+    
+    func comer(){
+        print("O elefante come muito")
+    }
+    
+}
+
+class noteBook{
+    var cor: String = "Preto"
+    var memoria: Int = 40
+    var bateria: Bool = true
+    var cabo: Bool = true
+    
+    func liga (){
+        print ("O notebook está com carga é igual a \(bateria)")
+    }
+    func capacidade (){
+        print ("O notebook está sem memoria")
+    }
+}
+var meuNotebook: noteBook = noteBook()
+meuNotebook.liga()
+meuNotebook.capacidade()
+print (meuNotebook.bateria)
+
+class Bicicleta {
+    var pneu: Bool = true
+    var selim: String = "Macio"
+    var corrente: Int = 2
+    var quadro: String = "Aluminio"
+    
+    func pedal (){
+        print("A bicicleta é feita para trilha")
+    }
+    func roubo (){
+        print("A bike tem um selim \(selim).")
+    }
+    func possuiPneu (){
+        print("A bike vem com o pneu? \(pneu)")
+    }
+}
+var minhaBike: Bicicleta = Bicicleta()
+print(minhaBike.pneu)
+minhaBike.roubo()
+minhaBike.possuiPneu()
+minhaBike.pedal()
+print(minhaBike.corrente)
+print(minhaBike.quadro)
+
+class sofa{
+    var tecido: String = "Linho"
+    var modelo: String = "Grande"
+    var almofadas: Int = 5
+    var temLugar: Bool = true
+    
+    func lavar(){
+        print("Sofa deve ser lavado sempre, pois seu material é \(tecido)")
+    }
+    func conservar(){
+        print("O sofa deve ser lavado")
+    }
+    func uso(){
+        print("O sofa possui \(almofadas) almofadas")
+    }
+    func local(){
+        print("O sofa possui lugares a vontade? \(temLugar)")
+    }
+}
+var meuSofa: sofa = sofa()
+print(meuSofa.tecido)
+print(meuSofa.modelo)
+meuSofa.lavar()
+meuSofa.conservar()
+meuSofa.local()
+meuSofa.uso()
