aa8a8be8053efb0d602166d5b92548e1eab22a42
Bernardo Aguiar Guimarães <bernardoguimaraes621@gmail.com>
2023-03-14 20:34:54 -0300

    atualizacoes

--- Heranca/Heranca/main.swift
+++ Heranca/Heranca/main.swift
@@ -91,7 +91,7 @@ class Tecnologia {
     }
     
     func conectarPessoa() {
-        print("A teclogia está conectado com as pessoas")
+        print("A tecnologia está conectado com as pessoas")
     }
     
     func ajudarPessoas() {
@@ -126,3 +126,59 @@ var meuCelular: Celular = Celular(marca: "Apple", modelo: "Iphone 14", ano: 2023
 
 //    "" -> String vazia
 //    nil -> SIGNIFICA NENHUM VALOR
+
+
+//Desafios de Construtores:
+// Nota: Vocês tem total liberdade de escolher o tipo das variáveis. Sugiro que usem tipos diversos.
+
+
+// Crie uma classe chamada "Livro" que tenha as propriedades "titulo", "autor" e "preço". Em seguida, crie uma subclasse chamada "LivroDigital" que herde da classe "Livro" e tenha uma propriedade adicional chamada "formato". Crie dois construtores para a classe "LivroDigital". O primeiro deve aceitar o "formato" como parâmetro enquanto o segundo não deve aceitá-lo. Quando o "formato" não for fornecido, defina-o como "PDF". Defina então um método chamado "analisarPreco" que receba o "titulo" e o "preço" do livro como parâmetros e imprima "Livro caro" se o preço for maior que 25 ou "Livro barato", caso contrário.
+
+class Livro {
+    var titulo: String
+    var autor: String
+    var preco: Double
+    
+    init(titulo: String, autor: String, preco: Double) {
+        self.titulo = titulo
+        self.autor = autor
+        self.preco = preco
+    }
+}
+
+class LivroDigital: Livro {
+    var formato: String //PDF
+    
+    init(titulo: String, autor: String, preco: Double, formato: String) {
+        self.formato = formato
+        super.init(titulo: titulo, autor: autor, preco: preco)
+    }
+    override convenience init(titulo: String, autor: String, preco: Double) {
+        
+        self.init(titulo: titulo, autor: autor, preco: preco)
+        
+        
+    }
+    func analisarPreco() {
+        if preco > 25.0{
+            print("Livro caro")
+        }else{
+            print("Livro barato")
+        }
+    }
+    
+}
+
+var eraUmaVez: LivroDigital = LivroDigital(titulo: "Era uma vez", autor: "Jose", preco: 90.0)
+//analisarPreco()
+
+//LivroDigital = LivroDigital(titulo: "Era uma vez", autor: "Jose", preco: 90.0)
+
+
+
+
+// Crie uma classe chamada "FormaGeometrica" com as propriedades "largura" e "altura". Em seguida, crie uma subclasse chamada "Retangulo" que tenha um método "calcularArea" que calcula a área do retângulo (largura * altura). Certifique-se de que o construtor da subclasse chame o construtor da classe pai para definir a largura e a altura e imprima o resultado.
+
+
+// Crie uma classe chamada "ContaBancaria" com as propriedades "saldo" e "tipo". Em seguida, crie uma subclasse chamada "ContaCorrente" que tenha uma propriedade adicional chamada "limite" e um método "atualizarSaldo" que adiciona o limite ao saldo da conta. Certifique-se de que o construtor da subclasse chame o construtor da classe pai para definir o "saldo" e o "tipo". Você pode ainda criar um método "imprimirSaldo" que imprime o saldo atualizado da conta.
+
