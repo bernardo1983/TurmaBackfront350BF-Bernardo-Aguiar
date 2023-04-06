957b409e932087fabf541d3ec0f2aaa744fd2f3c
Bernardo Aguiar Guimarães <bernardoguimaraes621@gmail.com>
2023-04-06 09:58:37 -0300

    no message

---
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Bernardo Aguiar Guimarães on 28/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
