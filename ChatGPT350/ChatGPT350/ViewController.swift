c86c46b9bf6999fdec76e1da38a7dfa39eb64b13
Bernardo Aguiar Guimarães <bernardoguimaraes621@gmail.com>
2023-05-11 19:49:37 -0300

    Criacao de projeto Chat GPT

---
+++ ChatGPT350/ChatGPT350/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPT350
+//
+//  Created by Bernardo Aguiar Guimarães on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
